d68f15fb8434656063bb5aa20c815b309a0db143
Sunil PN <sunildiroi@gmail.com>
2026-03-24 10:38:14 +0530

    feat(diviso): initialize FixMyArea project structure and JPA entities

---
+++ fixMyArea/src/main/java/com/diviso/fixMyArea/FixMyAreaApplication.java
@@ -0,0 +1,13 @@
+package com.diviso.fixMyArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixMyAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixMyAreaApplication.class, args);
+	}
+
+}
---
+++ fixMyArea/src/test/java/com/diviso/fixMyArea/FixMyAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diviso.fixMyArea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMyAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
