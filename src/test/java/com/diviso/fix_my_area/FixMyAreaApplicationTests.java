fb4567e74dcf9302f157d094f802ef9e7baaf9d1
harikesh <harikeshhari009@gmail.com>
2026-04-13 10:13:43 +0530

    chore: checkpoint untracked files

---
+++ src/test/java/com/diviso/fix_my_area/FixMyAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diviso.fix_my_area;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMyAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
