8691d7efd208ba72391e70b823e21d91354787bc
Sunil PN <sunildiroi@gmail.com>
2026-05-27 15:26:55 +0530

    unwanted files removed

---
+++ project/frontend/fix_my_area/app/issue-details-view/components/IssueDetailViewCard.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+export default function IssueDetailViewCard(){
+
+
+    type  IssueDetail = {
+
+    issueId:number
+    openMap:string
+    ward:number
+    affectedArea:string
+    deadline:number
+    support:number
+    concerns:number
+    suggestions:number
+
+    }
+
+
+    return(
+
+<div>
+
+{
+
+
+    }
+
+<IssueDetailViewCard
+
+
+
+/>
+
+
+
+
+</div>
+    );
+
+
+    }
